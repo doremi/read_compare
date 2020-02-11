0476f2e2aeb41a06eff65241cd68133546686039
Shane Lin <253692+doremi@users.noreply.github.com>
2020-02-11 09:00:09 +0800

    If read failed, reopen file descriptor
    * And add another argument to write anything we read

--- read_compare.c
+++ read_compare.c
@@ -8,6 +8,21 @@
 
 #define BLOCK_SIZE (2048)
 
+static char *dev_filename = NULL;
+static int g_devfd = -1;
+
+static int dev_close_and_reopen() {
+    if (g_devfd > 0) {
+        close(g_devfd);
+    }
+    g_devfd = open(dev_filename, O_RDONLY);
+    if (!g_devfd) {
+        perror(dev_filename);
+        return -1;
+    }
+    return 0;
+}
+
 static int read_block(int fd, void *buf, const off_t off_blocks, const int retry) {
     for (int i = 0; i < retry; ++i) {
         if (pread(fd, buf, BLOCK_SIZE, (off_blocks * BLOCK_SIZE)) == BLOCK_SIZE) {
@@ -15,33 +30,43 @@ static int read_block(int fd, void *buf, const off_t off_blocks, const int retry
         } else {
             perror("pread");
             printf("Try %d: Read error on block %ld\n", i, off_blocks);
-            sleep(1);
+            close(fd);
+            fd = -1;
+            fd = open(dev_filename, O_RDONLY);
+            if (!fd) {
+                perror("reopen");
+                return -1;
+            }
         }
     }
     return -1;
 }
 
-static int read_and_compare_block(const int devfd, const void *iso_mem, const off_t off_blocks) {
+static int read_and_compare_block(const void *iso_mem, const off_t off_blocks, const int isofd) {
     const int retry = 3;
     char devbuf[BLOCK_SIZE] = "";
     const void *target = (char*)iso_mem + (off_blocks * BLOCK_SIZE);
 
     for (int i = 0; i < retry; ++i) {
-        if (read_block(devfd, &devbuf, off_blocks, retry) != 0) {
-            printf("Read failed!\n");
+        if (read_block(g_devfd, &devbuf, off_blocks, retry) != 0) {
+            printf("Read failed, fd: %d\n", g_devfd);
             return -1;
         }
         if (memcmp(&devbuf, target, BLOCK_SIZE) == 0) {
             printf("Read and Compare OK on block %ld\r", off_blocks);
+            write(isofd, &devbuf, BLOCK_SIZE);
             return 0;
         } else {
             printf("\nTry %d: Read and Compare ERROR on blocks %ld\n", i, off_blocks);
+            printf("Old fd: %d, ", g_devfd);
+            dev_close_and_reopen();
+            printf("reopen fd: %d\n", g_devfd);
         }
     }
     return -1;
 }
 
-void read_and_compare(const char *devname, const char *isoname) {
+void read_and_compare(const char *devname, const char *isoname, const char *output) {
     const off_t size = 7851737088;
     const off_t total_blocks = size / BLOCK_SIZE;
 
@@ -50,6 +75,7 @@ void read_and_compare(const char *devname, const char *isoname) {
         perror(devname);
         return;
     }
+    g_devfd = devfd;
 
     int isofd = open(isoname, O_RDONLY);
     if (!isofd) {
@@ -66,7 +92,6 @@ void read_and_compare(const char *devname, const char *isoname) {
         return;
     }
 
-    /*
     int outfd = open(output, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
     if (!outfd) {
         perror(output);
@@ -74,10 +99,9 @@ void read_and_compare(const char *devname, const char *isoname) {
         close(isofd);
         return;
     }
-    */
 
     for (off_t curr_blocks = 0; curr_blocks < total_blocks; ++curr_blocks) {
-        if (read_and_compare_block(devfd, iso_mem, curr_blocks) == -1) {
+        if (read_and_compare_block(iso_mem, curr_blocks, outfd) == -1) {
             break;
         }
     }
@@ -86,19 +110,19 @@ void read_and_compare(const char *devname, const char *isoname) {
     munmap(iso_mem, size);
     close(devfd);
     close(isofd);
-    //    close(outfd);
+    close(outfd);
 }
 
 int main(int argc, char **argv) {
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s input1 input2\n", argv[0]);
+    if (argc != 4) {
+        fprintf(stderr, "Usage: %s input1 input2 output\n", argv[0]);
         return 2;
     }
 
-    struct stat st;
-    lstat(argv[1], &st);
-    printf("dev st_size: %ld\n", st.st_size);
-
-    read_and_compare(argv[1], argv[2]);
+    printf("input1: %s\n", argv[1]);
+    printf("input2: %s\n", argv[2]);
+    printf("output: %s\n", argv[3]);
+    dev_filename = argv[1];
+    read_and_compare(argv[1], argv[2], argv[3]);
     return 0;
 }
