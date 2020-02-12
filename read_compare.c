b53a5994f80334eeabe6e738e745eb0b79d752de
Shane Lin <253692+doremi@users.noreply.github.com>
2020-02-12 14:28:41 +0800

    Add support for Direct I/O

--- read_compare.c
+++ read_compare.c
@@ -1,4 +1,6 @@
+#define _GNU_SOURCE
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
@@ -25,11 +27,12 @@ static int dev_close_and_reopen() {
 
 static int read_block(int fd, void *buf, const off_t off_blocks, const int retry) {
     for (int i = 0; i < retry; ++i) {
-        if (pread(fd, buf, BLOCK_SIZE, (off_blocks * BLOCK_SIZE)) == BLOCK_SIZE) {
+        ssize_t read_size = pread(fd, buf, BLOCK_SIZE, (off_blocks * BLOCK_SIZE));
+        if (read_size == BLOCK_SIZE) {
             return 0;
         } else {
             perror("pread");
-            printf("Try %d: Read error on block %ld\n", i, off_blocks);
+            printf("Try %d: Read error on block %ld, read size: %ld\n", i, off_blocks, read_size);
             close(fd);
             fd = -1;
             fd = open(dev_filename, O_RDONLY);
@@ -42,25 +45,24 @@ static int read_block(int fd, void *buf, const off_t off_blocks, const int retry
     return -1;
 }
 
-static int read_and_compare_block(const void *iso_mem, const off_t off_blocks, const int isofd) {
+static int read_and_compare_block(const void* const iso_mem, const off_t off_blocks, void* const devbuf, const int isofd) {
     const int retry = 3;
-    char devbuf[BLOCK_SIZE] = "";
     const void *target = (char*)iso_mem + (off_blocks * BLOCK_SIZE);
 
     for (int i = 0; i < retry; ++i) {
-        if (read_block(g_devfd, &devbuf, off_blocks, retry) != 0) {
+        if (read_block(g_devfd, devbuf, off_blocks, retry) != 0) {
             printf("Read failed, fd: %d\n", g_devfd);
             return -1;
         }
-        if (memcmp(&devbuf, target, BLOCK_SIZE) == 0) {
+        if (memcmp(devbuf, target, BLOCK_SIZE) == 0) {
             printf("Read and Compare OK on block %ld\r", off_blocks);
-            write(isofd, &devbuf, BLOCK_SIZE);
+            write(isofd, devbuf, BLOCK_SIZE);
             return 0;
         } else {
             printf("\nTry %d: Read and Compare ERROR on blocks %ld\n", i, off_blocks);
-            printf("Old fd: %d, ", g_devfd);
-            dev_close_and_reopen();
-            printf("reopen fd: %d\n", g_devfd);
+            //printf("Old fd: %d, ", g_devfd);
+            //            dev_close_and_reopen();
+            //printf("reopen fd: %d\n", g_devfd);
         }
     }
     return -1;
@@ -69,10 +71,16 @@ static int read_and_compare_block(const void *iso_mem, const off_t off_blocks, c
 void read_and_compare(const char *devname, const char *isoname, const char *output) {
     const off_t size = 7851737088;
     const off_t total_blocks = size / BLOCK_SIZE;
+    void *devbuf = aligned_alloc(BLOCK_SIZE, BLOCK_SIZE);
+    if (!devbuf) {
+        perror("aligned_alloc");
+        return;
+    }
 
-    int devfd = open(devname, O_RDONLY);
+    int devfd = open(devname, O_RDONLY | O_DIRECT);
     if (!devfd) {
         perror(devname);
+        free(devbuf);
         return;
     }
     g_devfd = devfd;
@@ -80,6 +88,7 @@ void read_and_compare(const char *devname, const char *isoname, const char *outp
     int isofd = open(isoname, O_RDONLY);
     if (!isofd) {
         perror(isoname);
+        free(devbuf);
         close(devfd);
         return;
     }
@@ -87,6 +96,7 @@ void read_and_compare(const char *devname, const char *isoname, const char *outp
     void *iso_mem = mmap(NULL, size, PROT_READ, MAP_SHARED, isofd, 0);
     if (iso_mem == MAP_FAILED) {
         perror("iso mmap");
+        free(devbuf);
         close(devfd);
         close(isofd);
         return;
@@ -95,19 +105,21 @@ void read_and_compare(const char *devname, const char *isoname, const char *outp
     int outfd = open(output, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
     if (!outfd) {
         perror(output);
+        free(devbuf);
         close(devfd);
         close(isofd);
         return;
     }
 
     for (off_t curr_blocks = 0; curr_blocks < total_blocks; ++curr_blocks) {
-        if (read_and_compare_block(iso_mem, curr_blocks, outfd) == -1) {
+        if (read_and_compare_block(iso_mem, curr_blocks, devbuf, outfd) == -1) {
             break;
         }
     }
 
     printf("\nOK\n");
     munmap(iso_mem, size);
+    free(devbuf);
     close(devfd);
     close(isofd);
     close(outfd);
